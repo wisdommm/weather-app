8267cb0f032ec2d3e4e73b3178b79d1cfd7670f1
wanyan.hzh <wanyan.hzh@alibaba-inc.com>
2022-03-26 23:56:13 +0800

    stash

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,9 +4,11 @@ this.addEventListener('install', function (event) {
       caches.open('last').then(function (cache) {
         return cache.addAll([
           "/weather-test/",
-          "/weather-test/index.html",
+          "/weather-test/favicon.ico",
+          "/weather-test/manifest.json",
+
           "/weather-test/static/css/main.e0ce3286.css",
-          "/weather-test/static/js/main.7cd6d280.js",
+          "/weather-test/static/js/main.839df8d1.js",
           "/weather-test/static/js/787.13d76ca7.chunk.js",
           "/weather-test/static/media/sun.2857d53e5fcc0124a24f.png",
           "/weather-test/static/media/snow.2147bcf6dd11a10ae469.png",
@@ -14,13 +16,12 @@ this.addEventListener('install', function (event) {
           "/weather-test/static/media/overcast.e5f7d1e5452f24342990.png",
           "/weather-test/static/media/rain.f848fca9b7d52957bc68.png",
           "/weather-test/static/media/cloud.974591b65bdd465e882d.png",
+          "/weather-test/index.html",
           "/weather-test/static/css/main.e0ce3286.css.map",
-          "/weather-test/static/js/main.7cd6d280.js.map",
+          "/weather-test/static/js/main.839df8d1.js.map",
           "/weather-test/static/js/787.13d76ca7.chunk.js.map",
           "/weather-test/static/css/main.e0ce3286.css",
-          "/weather-test/static/js/main.7cd6d280.js",
-          "/weather-test/favicon.ico",
-          "/weather-test/manifest.json"
+          "/weather-test/static/js/main.839df8d1.js"
         ]);
       })
     );
