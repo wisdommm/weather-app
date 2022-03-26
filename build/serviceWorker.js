7d25ce81bfb239c7dd4d91534f18d1de13f3ed40
wanyan.hzh <wanyan.hzh@alibaba-inc.com>
2022-03-26 23:41:35 +0800

    stash

--- build/serviceWorker.js
+++ build/serviceWorker.js
@@ -6,8 +6,7 @@ this.addEventListener('install', function (event) {
           '/weather-test/',
           '/weather-test/index.html',
           '/weather-test/static/js/main.2db67bc8.js',
-          '/weather-test/static/js/main.2db67bc8.js.map',
-          '/weather-test/static/js/787.13d76ca7.chunk.js.map',
+          '/weather-test/static/css/main.e0ce3286.css',
           '/weather-test/static/js/787.13d76ca7.chunk.js',
           '/weather-test/static/media/sun.2857d53e5fcc0124a24f.png',
           '/weather-test/static/media/snow.2147bcf6dd11a10ae469.png',
@@ -16,24 +15,8 @@ this.addEventListener('install', function (event) {
           '/weather-test/static/media/rain.f848fca9b7d52957bc68.png',
           '/weather-test/static/media/cloud.974591b65bdd465e882d.png',
           '/weather-test/static/css/main.e0ce3286.css.map',
-          '/weather-test/static/css/main.e0ce3286.css',
-          "/weather-test/static/css/main.e0ce3286.css",
-          "/weather-test/static/js/main.7cd6d280.js",
-          "/weather-test/static/js/787.13d76ca7.chunk.js",
-          "/weather-test/static/media/sun.2857d53e5fcc0124a24f.png",
-          "/weather-test/static/media/snow.2147bcf6dd11a10ae469.png",
-          "/weather-test/static/media/today.9071f7f21653d1814559.png",
-          "/weather-test/static/media/overcast.e5f7d1e5452f24342990.png",
-          "/weather-test/static/media/rain.f848fca9b7d52957bc68.png",
-          "/weather-test/static/media/cloud.974591b65bdd465e882d.png",
-          "/weather-test/index.html",
-          "/weather-test/static/css/main.e0ce3286.css.map",
-          "/weather-test/static/js/main.7cd6d280.js.map",
-          "/weather-test/static/js/787.13d76ca7.chunk.js.map",
-          "/weather-teststatic/css/main.e0ce3286.css",
-          "/weather-teststatic/js/main.7cd6d280.js"
-
-
+          '/weather-test/static/js/main.2db67bc8.js.map',
+          '/weather-test/static/js/787.13d76ca7.chunk.js.map',
         ]);
       })
     );
