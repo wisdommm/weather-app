d03288e33889f90104dd2c7279857f4402865ac5
wanyan.hzh <wanyan.hzh@alibaba-inc.com>
2022-03-27 14:39:26 +0800

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,19 +6,19 @@
           "/weather-test/favicon.ico",
           "/weather-test/manifest.json",
 
-          "/weather-test/static/css/main.0f36e62e.css",
-          "/weather-test/static/js/main.c1992069.js",
-          "/weather-test/static/js/787.13d76ca7.chunk.js",
-          "/weather-test/static/media/sun.2857d53e5fcc0124a24f.png",
-          "/weather-test/static/media/snow.2147bcf6dd11a10ae469.png",
-          "/weather-test/static/media/overcast.e5f7d1e5452f24342990.png",
-          "/weather-test/static/media/rain.f848fca9b7d52957bc68.png",
-          "/weather-test/static/media/cloud.974591b65bdd465e882d.png",
-          "/weather-test/static/media/today.2af6c7195788cc85089d.png",
-          "/weather-test/index.html",
-          "/weather-test/static/css/main.0f36e62e.css.map",
-          "/weather-test/static/js/main.c1992069.js.map",
-          "/weather-test/static/js/787.13d76ca7.chunk.js.map"
+          "/weather-app/static/css/main.0f36e62e.css",
+          "/weather-app/static/js/main.1aaf5e89.js",
+          "/weather-app/static/js/787.13d76ca7.chunk.js",
+          "/weather-app/static/media/sun.2857d53e5fcc0124a24f.png",
+          "/weather-app/static/media/snow.2147bcf6dd11a10ae469.png",
+          "/weather-app/static/media/overcast.e5f7d1e5452f24342990.png",
+          "/weather-app/static/media/rain.f848fca9b7d52957bc68.png",
+          "/weather-app/static/media/cloud.974591b65bdd465e882d.png",
+          "/weather-app/static/media/today.2af6c7195788cc85089d.png",
+          "/weather-app/index.html",
+          "/weather-app/static/css/main.0f36e62e.css.map",
+          "/weather-app/static/js/main.1aaf5e89.js.map",
+          "/weather-app/static/js/787.13d76ca7.chunk.js.map"
         ]);
       })
     );
